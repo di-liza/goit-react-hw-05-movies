65e529623cf19b6c7a5803303c1b8463f3fa4f27
di-liza <114363326+di-liza@users.noreply.github.com>
2023-05-27 23:37:08 +0000

    Deploying to gh-pages from @ di-liza/goit-react-hw-05-movies@af2dffb1af256a1274cad8079ef9e4fea7d8d4e0 🚀

--- static/js/256.24314d33.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[256],{256:function(e,t,r){r.r(t);r(791);var n=r(184);t.default=function(){return(0,n.jsx)("div",{children:(0,n.jsx)("h1",{children:"NotFound"})})}}}]);
-//# sourceMappingURL=256.24314d33.chunk.js.map
