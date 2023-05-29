2342f8ffad43ceaeb85bf4b98ddea2e70b97631a
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-05-29 23:24:20 +0300

    add pagination to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
+// import '../../node_modules/react-paginate/theme/basic/react-paginate.css';
+
 import { getTrendingMovies } from 'servises';
-import { MoviesList, Hero, SwButtons, Loader } from 'components';
+// SwButtons;
+import { MoviesList, Hero, Loader } from 'components';
 
-function Home() {
+function Home({ itemsPerPage }) {
   const [searchParams, setSearchParams] = useSearchParams();
-
+  // const [itemOffset, setItemOffset] = useState(0);
   const searchPage = searchParams.get('page') ?? 1;
+
   const [movies, setMovies] = useState([]);
+  const [totalResults, setTotalResults] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
@@ -15,8 +22,11 @@ function Home() {
       if (searchPage >= 1) {
         try {
           setIsLoading(true);
-          const { results } = await getTrendingMovies(searchPage);
-          setMovies(results);
+          const data = await getTrendingMovies(searchPage);
+          console.log('data:', data);
+          setTotalPages(data.total_pages);
+          setTotalResults(data.total_results);
+          setMovies(data.results);
         } catch (error) {
           console.log('error:', error);
         } finally {
@@ -27,25 +37,41 @@ function Home() {
     getTrandMovies();
   }, [searchPage]);
 
-  const handlePageToggle = action => {
-    if (searchPage === 1 && action === 'prev')
-      return setSearchParams({ page: 1 });
+  // const handlePageToggle = action => {
+  //   if (searchPage === 1 && action === 'prev')
+  //     return setSearchParams({ page: 1 });
 
-    const prevPage = Number(searchParams.get('page')) || 1;
+  //   const prevPage = Number(searchParams.get('page')) || 1;
 
-    if (action === 'prev') {
-      setSearchParams({ page: prevPage - 1 });
-    } else {
-      setSearchParams({ page: prevPage + 1 });
-    }
+  //   if (action === 'prev') {
+  //     setSearchParams({ page: prevPage - 1 });
+  //   } else {
+  //     setSearchParams({ page: prevPage + 1 });
+  //   }
+  // };
+  const handlePageClick = event => {
+    // const newOffset = (event.selected * 20) % movies.length;
+    // console.log(
+    //   `User requested page number ${event.selected}, which is offset ${newOffset}`
+    // );
+    setSearchParams({ page: event.selected + 1 });
   };
 
   return (
     <div style={{ paddingBottom: '20px' }}>
       <Hero />
       <MoviesList movies={movies} path={'movies/'} />
-      <SwButtons onClickSwichBtn={handlePageToggle} page={searchPage} />
+      {/* <SwButtons onClickSwichBtn={handlePageToggle} page={searchPage} /> */}
       {isLoading && <Loader />}
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={totalPages}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+      />
     </div>
   );
 }
