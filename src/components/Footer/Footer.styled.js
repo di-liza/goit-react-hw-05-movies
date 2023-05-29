b40ace81dbd20be01a4e9c30416d3c87358d463d
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-05-30 00:16:31 +0300

    return

--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -2,9 +2,9 @@ const { default: styled } = require('@emotion/styled');
 
 export const StyledFooter = styled.footer`
   background-color: #1d0202;
-  padding: 10px;
+  padding: 20px;
 
-  position: relative;
+  position: absolute;
   width: 100%;
   bottom: 0;
 
