8ef11abb8a5d3386b200d7022b59e93d95323495
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-05-30 00:14:02 +0300

    fix footer size

--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -2,14 +2,13 @@ const { default: styled } = require('@emotion/styled');
 
 export const StyledFooter = styled.footer`
   background-color: #1d0202;
-  padding: 20px;
+  padding: 10px;
 
   position: relative;
   width: 100%;
   bottom: 0;
 
   .footer-bottom {
-    margin-top: 20px;
     text-align: center;
     font-size: 14px;
     color: #666;
